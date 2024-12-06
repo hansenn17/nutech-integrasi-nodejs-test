936081a709c458f745860715a084ba8098b7be09
hansendjumari <hansen.djumari@gdplabs.id>
2024-12-07 04:01:58 +0700

    feat: init server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`);
+});
